24ef1dceeba5643a59af97620c9ed2a269893a0b
Soichiro Miki <smiki-tky@umin.ac.jp>
2017-06-08 15:43:35 +0900

    Add Modal

--- demo/demo.js
+++ demo/demo.js
@@ -7,10 +7,53 @@ import ReactDom from 'react-dom';
 
 import IconButton from '../lib/IconButton';
 import LoadingIndicator from '../lib/LoadingIndicator';
+import { alert, confirm, prompt } from '../lib/Modal';
+
+const onAlertClick = async () => {
+	await alert('Alert!');
+};
+
+const onConfirmClick = async () => {
+	const answer = await confirm('Are you sure?');
+	if (answer) {
+		await alert('You clicked OK');
+	} else {
+		await alert('You clicked Cancel');
+	}
+};
+
+const onPromptClick = async () => {
+	const answer = await prompt('Input something');
+	await alert('Your input is: ' + answer);
+}
 
 const Demo = () => <div>
-	<LoadingIndicator />
-	<IconButton icon='home'>Hello</IconButton>
+	<div>
+		<LoadingIndicator />
+	</div>
+	<div>
+		<IconButton
+			icon='exclamation-triangle'
+			bsStyle='warning'
+			onClick={onAlertClick}
+		>
+			Alert
+		</IconButton>
+		<IconButton
+			icon='question-circle'
+			bsStyle='info'
+			onClick={onConfirmClick}
+		>
+			Confirm
+		</IconButton>
+		<IconButton
+			icon='comment'
+			bsStyle='primary'
+			onClick={onPromptClick}
+		>
+			Prompt
+		</IconButton>
+	</div>
 </div>;
 
 ReactDom.render(
