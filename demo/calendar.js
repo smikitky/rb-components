8d0fa28fc7caf270d378132ef2ad2f70646c2597
Soichiro Miki <smiki-tky@umin.ac.jp>
2017-06-13 16:31:00 +0900

    Add RelativeDatePicker

--- demo/calendar.js
+++ demo/calendar.js
@@ -1,9 +1,26 @@
 import React from 'react';
 
 import Calendar from '../lib/Calendar';
+import RelativeDatePicker from '../lib/RelativeDatePicker';
 
-export default function CalendarDemo() {
-	return <div>
-		<Calendar />
-	</div>;
+export default class CalendarDemo extends React.PureComponent {
+	constructor(props) {
+		super(props);
+		this.state = { relativeValue: null };
+		this.relativeChange = this.relativeChange.bind(this);
+	}
+
+	relativeChange(value) {
+		this.setState({ relativeValue: value });
+	}
+
+	render() {
+		return <div>
+			<Calendar />
+			<RelativeDatePicker
+				value={this.state.relativeValue}
+				onChange={this.relativeChange}
+			/>
+		</div>;
+	}
 }
