f259a7814b1c70540bef3c80b7cfb1a9ea90a3be
Soichiro Miki <smiki-tky@umin.ac.jp>
2017-06-24 18:58:55 +0900

    Introduce DropdownDatePicker

--- demo/calendar.js
+++ demo/calendar.js
@@ -1,15 +1,27 @@
 import React from 'react';
 
 import Calendar from '../lib/Calendar';
+import DropdownDatePicker from '../lib/DropdownDatePicker';
 import RelativeDatePicker from '../lib/RelativeDatePicker';
 import DateRangePicker from '../lib/DateRangePicker';
 import ValuePreview from './value-preview';
+
+const DropdownDatePickerDemo = props => <div>
+	<h4>Default</h4>
+	<DropdownDatePicker {...props} />
+	<h4>Blocked</h4>
+	<DropdownDatePicker {...props} block bsSize='xs' bsStyle='primary' />
+</div>;
 
 const CalendarDemo = () => {
 	return <div>
 		<h3>Clickable Calendar</h3>
 		<ValuePreview event='onDateClick'>
 			<Calendar />
+		</ValuePreview>
+		<h3>Dropdown Date Picker</h3>
+		<ValuePreview initialValue='2017-01-01'>
+			<DropdownDatePickerDemo />
 		</ValuePreview>
 		<h3>Relative Date Picker</h3>
 		<ValuePreview initialValue={null}>
