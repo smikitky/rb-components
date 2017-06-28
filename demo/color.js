985df2d677d999f53b44b62d138745f59ac5ccc7
Soichiro Miki <smiki-tky@umin.ac.jp>
2017-06-28 14:18:27 +0900

    Rewrite ColorPicker using Dropdown

--- demo/color.js
+++ demo/color.js
@@ -3,11 +3,21 @@ import React from 'react';
 import ColorPicker, { ColorPalette } from '../lib/ColorPicker';
 import ValuePreview from './value-preview';
 
+const ColorPickers = props => <div>
+	<ColorPicker showColorCode {...props} />
+	&ensp;
+	<ColorPicker bsSize='xs' {...props} />
+	&ensp;
+	<ColorPicker noCaret {...props} />
+	&ensp;
+	<ColorPicker noCaret showColorCode {...props} />
+</div>;
+
 const ColorDemo = () => {
 	return <div>
-		<h3>Color Picker</h3>
+		<h3>Dropdown Color Pickers</h3>
 		<ValuePreview initialValue={'#ff0000'} >
-			<ColorPicker showColorCode={true} />
+			<ColorPickers />
 		</ValuePreview>
 		<h3>Static Color Palette</h3>
 		<ValuePreview initialValue={'#ff0000'}>
