8de01a8a6901d5989ca190810265f0a9721a5790
Soichiro Miki <smiki-tky@umin.ac.jp>
2018-02-02 12:54:18 +0900

    Output to `dist` folder

--- webpack.config.js
+++ webpack.config.js
@@ -2,42 +2,37 @@ const path = require('path');
 const webpack = require('webpack');
 
 module.exports = {
-	entry: {
-		demo: [
-			'react-hot-loader/patch',
-			path.join(__dirname, 'demo', 'demo.js')
-		]
-	},
-	output: {
-		path: path.join(__dirname, 'build'),
-		filename: '[name].js'
-	},
-	module: {
-		rules: [
-			{
-				test: /\.js$/,
-				exclude: /node_modules/,
-				use: {
-					loader: 'babel-loader',
-					options: {
-						presets: ['react'],
-						plugins: [
-							'transform-es2015-classes',
-							'transform-object-rest-spread',
-							'react-hot-loader/babel'
-						]
-					}
-				}
-			}
-		]
-	},
-	plugins: [
-		new webpack.HotModuleReplacementPlugin()
-	],
-	devServer: {
-		host: process.env.IP ? process.env.IP : 'localhost',
-		disableHostCheck: true,
-		hot: true,
-		compress: true
-	}
+  entry: {
+    demo: ['react-hot-loader/patch', path.join(__dirname, 'demo', 'demo.js')]
+  },
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: '[name].js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['react'],
+            plugins: [
+              'transform-es2015-classes',
+              'transform-object-rest-spread',
+              'react-hot-loader/babel'
+            ]
+          }
+        }
+      }
+    ]
+  },
+  plugins: [new webpack.HotModuleReplacementPlugin()],
+  devServer: {
+    host: process.env.IP ? process.env.IP : 'localhost',
+    disableHostCheck: true,
+    hot: true,
+    compress: true
+  }
 };
