378610b845c90ebb475153fecab4ca28439d6e61
Soichiro Miki <smiki-tky@umin.ac.jp>
2018-02-06 21:33:23 +0900

    Add initial implementation of GradientEditor

--- demo/color.js
+++ demo/color.js
@@ -1,6 +1,7 @@
 import React from 'react';
 
 import ColorPicker, { ColorPalette } from '../lib/ColorPicker';
+import GradationEditor from '../lib/GradationEditor';
 import ValuePreview from './value-preview';
 
 const ColorPickers = props => (
@@ -20,6 +21,12 @@ const ColorPickers = props => (
   </div>
 );
 
+const gradationInitialValue = [
+  { position: 0, color: '#ff0000', alpha: 1 },
+  { position: 50, color: '#00ff00', alpha: 0.5 },
+  { position: 100, color: '#0000ff', alpha: 1 }
+];
+
 const ColorDemo = () => {
   return (
     <div>
@@ -30,6 +37,10 @@ const ColorDemo = () => {
       <h3>Static Color Palette</h3>
       <ValuePreview initialValue={'#ff0000'} canDisable>
         <ColorPalette />
+      </ValuePreview>
+      <h3>Gradation Editor</h3>
+      <ValuePreview initialValue={gradationInitialValue} canDisable>
+        <GradationEditor />
       </ValuePreview>
     </div>
   );
