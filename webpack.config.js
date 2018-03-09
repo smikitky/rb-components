56401f6a0b829b4e005f73a96ce0c257d14da0e3
Soichiro Miki <smiki-tky@umin.ac.jp>
2018-03-09 16:28:10 +0900

    Simplify webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -6,10 +6,6 @@ module.exports = {
   entry: {
     demo: ['react-hot-loader/patch', path.join(__dirname, 'demo', 'demo.js')]
   },
-  output: {
-    path: path.join(__dirname, 'dist'),
-    filename: '[name].js'
-  },
   module: {
     rules: [
       {
@@ -27,7 +23,7 @@ module.exports = {
   },
   plugins: [new webpack.HotModuleReplacementPlugin()],
   devServer: {
-    host: process.env.IP ? process.env.IP : 'localhost',
+    host: process.env.IP || 'localhost',
     disableHostCheck: true,
     hot: true,
     compress: true
