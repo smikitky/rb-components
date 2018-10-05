19854e321393c867cd2f9cc224b11e3615e05cba
Soichiro Miki <smiki-tky@umin.ac.jp>
2018-10-05 16:18:12 +0900

    Support requiredProperties

--- demo/json-schema-editor.js
+++ demo/json-schema-editor.js
@@ -6,10 +6,15 @@ const schema = {
   type: 'object',
   properties: {
     age: { type: 'integer' },
-    zip: { type: 'string', pattern: '^\\d{3}\\-\\d{4}$' },
-    job: { enum: ['manager', 'director'] },
+    hex: { type: 'string', pattern: '^[a-fA-F0-9]+$' },
+    job: { enum: ['manager', 'director', 100] },
     chk: { type: 'boolean' }
   }
+};
+
+const schema2 = {
+  ...schema,
+  requiredProperties: ['age', 'hex', 'job', 'chk']
 };
 
 const JsonSchemaEditorDemo = () => {
@@ -20,8 +25,26 @@ const JsonSchemaEditorDemo = () => {
         <code>JsonSchemaEditor</code> is a key-value editor that supports a very
         limited subset of JSON Schema.
       </p>
+      <ul>
+        <li>
+          The root must be <code>type: object</code> with{' '}
+          <code>properties: ...</code>.
+        </li>
+        <li>
+          Each entry in <code>properties</code> must either 1) have an
+          appropriate scalar type (<code>string</code>, <code>number</code>,{' '}
+          <code>integer</code>, <code>boolean</code>
+          ), or 2) have <code>enum</code>.
+        </li>
+      </ul>
+
+      <h3>Optional Values</h3>
       <ValuePreview initialValue={{}} canDisable>
         <JsonSchemaEditor schema={schema} />
+      </ValuePreview>
+      <h3>Requried Values</h3>
+      <ValuePreview initialValue={{}} canDisable>
+        <JsonSchemaEditor schema={schema2} />
       </ValuePreview>
     </div>
   );
