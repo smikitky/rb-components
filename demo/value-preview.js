5b8cf67e210946ea100df258cfa82dfb26485931
Soichiro Miki <smiki-tky@umin.ac.jp>
2019-03-10 12:01:15 +0900

    Use hooks for some components

--- demo/value-preview.js
+++ demo/value-preview.js
@@ -1,4 +1,4 @@
-import React, { cloneElement, Fragment } from 'react';
+import React, { cloneElement, Fragment, useState } from 'react';
 
 const DefaultPreviewer = props => {
   const { value } = props;
@@ -9,50 +9,44 @@ const DefaultPreviewer = props => {
  * Displays the specified component with its value
  * and maintains the value changes.
  */
-export default class ValuePreview extends React.PureComponent {
-  constructor(props) {
-    super(props);
-    this.state = { value: props.initialValue, disabled: false };
-    this.handleChange = this.handleChange.bind(this);
-    this.handleDisableClick = this.handleDisableClick.bind(this);
-  }
+const ValuePreview = props => {
+  const {
+    initialValue,
+    valueProp = 'value',
+    event = 'onChange',
+    canDisable = false,
+    children,
+    previewer: Previewer = DefaultPreviewer
+  } = props;
+  const [value, setValue] = useState(initialValue);
+  const [disabled, setDisabled] = useState(false);
 
-  handleChange(value) {
-    this.setState({ value });
-  }
+  const handleDisableClick = () => {
+    setDisabled(d => !d);
+  };
 
-  handleDisableClick() {
-    this.setState({ disabled: !this.state.disabled });
-  }
+  const child = cloneElement(children, {
+    [valueProp]: value,
+    [event]: setValue,
+    disabled
+  });
 
-  render() {
-    const {
-      valueProp = 'value',
-      event = 'onChange',
-      canDisable = false,
-      children,
-      previewer: Previewer = DefaultPreviewer
-    } = this.props;
-    const child = cloneElement(children, {
-      [valueProp]: this.state.value,
-      [event]: this.handleChange,
-      disabled: this.state.disabled
-    });
-    return (
-      <div>
-        {canDisable && (
-          <div>
-            <label>
-              <input type="checkbox" onClick={this.handleDisableClick} />
-              disabled
-            </label>
-          </div>
-        )}
-        {child}
-        <div className="value">
-          <Previewer value={this.state.value} />
+  return (
+    <div>
+      {canDisable && (
+        <div>
+          <label>
+            <input type="checkbox" onClick={handleDisableClick} />
+            disabled
+          </label>
         </div>
+      )}
+      {child}
+      <div className="value">
+        <Previewer value={value} />
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
+
+export default ValuePreview;
